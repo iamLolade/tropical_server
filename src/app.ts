cf3cd7a0a8b7bbf7a34323e9deb1b721aeb02379
iamLolade <adeniyiotemade@gmail.com>
2022-02-11 02:25:36 +0100

    Created database and connected via mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,27 @@
 import express from "express";
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+
 import cors from "cors";
+
+dotenv.config();
 
 //express app
 const app = express();
+
 
 //middleware
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
 //port
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`Listening on port: ${port}`))
+//connect to database
+const dbURI = process.env.dbURI as string
+mongoose.connect(dbURI)
+    .then(() => app.listen(port, () => console.log(`Listening on port: ${port}`)))
+    .then(() => console.log("Connected to database"))
+    .catch(error => console.log(error))
